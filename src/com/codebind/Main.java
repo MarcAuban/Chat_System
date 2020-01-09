ba852bc958907267e7b71485e99762d3ef232504
Charles <charles.dubois031@gmail.com>
2020-01-09 13:27:12 +0100

    main réparé

--- src/com/codebind/Main.java
+++ src/com/codebind/Main.java
@@ -3,18 +3,18 @@ package com.codebind;
 import javax.swing.*;
 
 public class Main {
-    public static void main (String[] args){
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new Login();
-            frame.setSize(300,300);
-            frame.setVisible(true);
-            frame.setLocationRelativeTo(null);
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        });
+	public static void main (String[] args){
+		SwingUtilities.invokeLater(() -> {
+			JFrame frame = new Login();
+			frame.setSize(300,300);
+			frame.setVisible(true);
+			frame.setLocationRelativeTo(null);
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		});
 
-    }
+	}
 }
